366293faa2a11aed3691f087d7381bb95562b638
AysenPetrov <65352041+AysenPetrov@users.noreply.github.com>
2020-08-04 00:27:58 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/Task1.java
@@ -0,0 +1,28 @@
+package com.geekbrains;
+
+import java.util.logging.Logger;
+
+
+public class Task1 {
+    private static final Logger logger = Logger.getLogger(Task1.class.getName());
+
+    public static void main(String[] args) {
+        final int[] res2 = process(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+        System.out.println(res2);
+    }
+
+    public static int[] process(int[] arrIn) {
+        int[] backArr = new int[2];
+        if(arrIn.length == 0) {
+            logger.info("В массиве нет элементов");
+            return arrIn;
+        }
+        for (int i = arrIn.length - 3; i >= 0 ; i--) {
+            if(arrIn[i] == 4) {
+                System.arraycopy(arrIn, (i+1), backArr,0,2);
+                return backArr;
+            }
+        }
+        throw new RuntimeException("В массиве нет ни одной 4-ки после которой были бы числа");
+    }
+}
---
+++ src/test/java/Task1Test.java
@@ -0,0 +1,47 @@
+import com.geekbrains.Task1;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.logging.*;
+
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertArrayEquals;
+
+@RunWith(Parameterized.class)
+public class Task1Test {
+    private static final Logger logger = Logger.getLogger(Task1.class.getName());
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{1,2,5,4,3,7,2}, new int[]{1,7}, "Case 1"},
+                {new int[]{7,3,5,8,2,1}, new int[]{1,3}, "Case 2"},
+                {new int[]{4,2,9}, new int[]{2,9}, "Case 3"}
+        });
+    }
+
+    private int[] arrOriginal;
+    private int[] arrExpected;
+    private String message;
+
+    public Task1Test(int[] arrOriginal, int[] arrExpected, String message) {
+        this.arrOriginal = arrOriginal;
+        this.arrExpected = arrExpected;
+        this.message = message;
+    }
+
+    @Test
+    public void task1() {
+        logger.info (message+": отправили массив: "+ Arrays.toString(arrOriginal)+" получили массив: "+ Arrays.toString(arrExpected));
+        assertArrayEquals("Заданный нами массив должен совпадать с полученным", arrExpected, Task1.process(arrOriginal));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testExceptionTask1() {
+       logger.info("Тест на появление RuntimeException");
+        Task1.process(new int[]{1,2,3});
+    }
+}
